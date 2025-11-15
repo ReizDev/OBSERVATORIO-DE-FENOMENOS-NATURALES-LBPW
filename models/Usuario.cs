fe82488b54c6622cffb0219ca2eafb77500d02d4
ReizDev <xpb23ruben@gmail.com>
2025-11-15 17:18:03 -0600

    Revert "."
    This reverts commit 61e7633cb84950f0afe882a25c205c16818778b0.

--- models/Usuario.cs
+++
@@ -1,8 +0,0 @@
-namespace MiProyecto.Models {
-    public class Usuario {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
-    }
-}
